08db8722e57c61204067422ee3c0295a0608f0b1
João Pedro Silva <jsangregorio@hotmail.com>
2017-03-16 18:00:56 -0300

    Load de construções

--- constructions/constructions.go
+++ constructions/constructions.go
@@ -1,1 +1,51 @@
 package constructions
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+type adjascent struct {
+	Builds   []string
+	Resource string
+	Bonus    []uint
+}
+type bonus struct {
+	Name  string
+	Value []uint
+}
+type construction struct {
+	Name         string
+	Image        string
+	Bonus        []bonus
+	ResourceCost [][2]uint
+	Adjascent    []adjascent
+	Score        []uint
+	Shared       string
+}
+
+var constructions []construction
+
+func RegisterConstructions() {
+	defer println("Construções carregadas!")
+
+	dir := "constructions/modules/"
+	modules, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Print("Erro ao ler o diretorio:", dir, " - ", err)
+	}
+	for _, module := range modules {
+		var element construction
+		fileContent, err := ioutil.ReadFile(string(dir + module.Name()))
+		if err != nil {
+			println("Erro ao ler o arquivo", dir, module.Name(), "-", err)
+		}
+
+		json.Unmarshal([]byte(string(fileContent)), &element)
+		constructions = append(constructions, element)
+		fmt.Printf("Construção carregada: %s\n", strings.Title(element.Name))
+	}
+
+}
--- main.go
+++ main.go
@@ -1,39 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-)
-
-type adjascent struct {
-	Builds   []string `json:"builds"`
-	Resource string   `json:"resource"`
-	Bonus    []uint   `json:"bonus"`
-}
-type bonus struct {
-	Name  string `json:"name"`
-	Value []uint `json:"value"`
-}
-type construction struct {
-	Name         string
-	Bonus        []bonus
-	ResourceCost [][2]uint
-	Adjascent    []adjascent `json:"adjascent"`
-	Score        []uint
-	Shared       string
-}
+import "./constructions"
 
 func main() {
-	jsons, err := ioutil.ReadFile("constructions.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var constructions []construction
-	errs := json.Unmarshal(jsons, &constructions)
-	if errs != nil {
-		fmt.Println("error:", errs)
-	}
-	fmt.Printf("%+v", constructions[0].Name)
 
+	constructions.RegisterConstructions()
 }
